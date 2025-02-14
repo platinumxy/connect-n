2424ec5e616285dacf1d5e8f14193eac266b6cf9
platinumxy <83834735+platinumxy@users.noreply.github.com>
2025-02-14 00:21:54 +0000

    Menu

--- connectn/__main__.py
+++ connectn/__main__.py
@@ -1,6 +1,26 @@
+from typing import Tuple, List, Dict, Callable
+
+FINISHED_MODES: List[Tuple[str, Callable[[], None]]] = [
+    ("nan", lambda: None), 
+]
 
 
-
-
+def main() -> None:
+    game_mode = menu(FINISHED_MODES)   
+    game_mode()
+    
+    
+def menu(lookup: List[Tuple[str, Callable[[], None]]]) -> Callable[[], None]:
+    print("Choose mode:")
+    for i, (name, _) in enumerate(lookup):
+        print(f"{i}: {name}")
+    
+    try: 
+        return lookup[int(input("> "))][1]
+    except (ValueError, IndexError):
+        print("Invalid choice\n")
+        return menu(lookup)
+    
+    
 if __name__ == '__main__':
-    print("Hello, world!")
+    main()
